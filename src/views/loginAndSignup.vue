e263609fea972ef5426bf5f2010cf8b9fe7f3dc5
Alzekri <alzekri@>
2022-11-23 22:13:37 +0200

    c3

---
+++ src/views/loginAndSignup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row">
+    <div class="col-md-7"><router-view></router-view></div>
+    <div class="col-md-5 col-img"></div>
+  </div>
+</template>
+<style lang="scss">
+body {
+}
+.col-img {
+  height: 100vh;
+  background-image: url(../assets/LandingImage2svg.svg);
+}
+</style>
